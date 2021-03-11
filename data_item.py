fixed some trivial bugs discovered when trying to run normalization.py

data_item.py
@@ -40,7 +40,7 @@ class DataItem :
             x2 /= self.HubbleParam
             self.BoxSize /= self.HubbleParam
 
-        self.displacement = sim_utils.get_displacement(x1, x2, BoxSize)
+        self.displacement = sim_utils.get_displacement(x1, x2, self.BoxSize)
 
         # we like to have channel dimension at the beginning
         # (channel is the 3d dimensionality)
@@ -88,7 +88,7 @@ class DataItem :
         r2 = r % 6  # this one labels the 6 transpositions
 
         # find the reflection
-        reflect_indices = [[,], [0,], [1,], [2,], [0,1,], [0,2,], [1,2,], [0,1,2,]][r1]
+        reflect_indices = [[], [0,], [1,], [2,], [0,1,], [0,2,], [1,2,], [0,1,2,]][r1]
         if r1 != 0 :
             self.__reflect(reflect_indices)
 
@@ -142,9 +142,9 @@ class InputTargetPair :
         # or a random integer
         if rand_int is None :
             r = np.random.default_rng(hash(self) % 2**32).integers(2**32)
-        else if isinstance(rand_int, callable) :
+        elif isinstance(rand_int, callable) :
             r = rand_int()
-        else if isinstance(rand_int, int) :
+        elif isinstance(rand_int, int) :
             r = rand_int
         else :
             raise RuntimeError('Invalid argument type of rand_int = {}'.format(rand_int))

normalization.py
@@ -28,7 +28,7 @@ def normalization(mode) :
 #{{{
     assert settings.USE_DENSITY # for convienience
 
-    dataset = Dataset(mode)
+    dataset = Dataset(mode, 0, 1) # we do this in a single process, so we set rank and world_size to 0, 1
 
     # make a first pass through the data, computing the variances
     # note that we're using the fact that mean(displacement) ~ 0
@@ -38,6 +38,7 @@ def normalization(mode) :
     var_density = 0.0
 
     for run_pair in dataset.run_pairs :
+        print('In normalization.py, first loop, {}'.format(run_pair))
         item = DataItem(run_pair[0]) # we only want to normalize the input
         var_displacement += np.var(item.displacement)
         var_density += np.var(item.density)
@@ -54,6 +55,7 @@ def normalization(mode) :
     avg_logdensity = 0.0
 
     for run_pair in dataset.run_pairs :
+        print('In normalization.py, second loop, {}'.format(run_pair))
         item = DataItem(run_pair[0])
         logdensity = np.log1p( item.density / DENSITY_FACTOR / math.sqrt(var_density) )
         var_logdensity += np.sum(logdensity**2) / logdensity.size

sim_utils.py
@@ -13,7 +13,7 @@ def load_particles(fname) :
     """
 #{{{ 
     with h5py.File(fname, 'r') as f :
-        x = f['PartType1/Coordinates'][np.argsort(f['PartType1/ParticleIDs'][...]), :]
+        x = f['PartType1/Coordinates'][...][np.argsort(f['PartType1/ParticleIDs'][...]), :]
 
     assert x.shape[0] == settings.NSIDE**3
     assert x.shape[1] == 3
@@ -22,14 +22,14 @@ def load_particles(fname) :
     assert factor * settings.GLASS_SIDE == settings.NSIDE
 
     x = x.reshape((*[factor,]*3, *[settings.GLASS_SIDE,]*3, 3))
-    out = np.empty((*[settings.NSIDE,]*3))
+    out = np.empty((*[settings.NSIDE,]*3, 3))
 
     for ii in range(factor) :
         for jj in range(factor) :
             for kk in range(factor) :
                 out[ii*settings.GLASS_SIDE : (ii+1)*settings.GLASS_SIDE,
                     jj*settings.GLASS_SIDE : (jj+1)*settings.GLASS_SIDE,
-                    kk*settings.GLASS_SIDE : (kk+1)*settings.GLASS_SIDE, :] = x[ii, jj, kk, ...]
+                    kk*settings.GLASS_SIDE : (kk+1)*settings.GLASS_SIDE, ...] = x[ii, jj, kk, ...]
 
     del x
     return out
@@ -40,6 +40,7 @@ def get_displacement(x1, x2, box_size) :
     Computes the displacement field x2-x1, taking into account
     periodic boundary conditions
     """
+    # TODO check if this function is correct!
 #{{{
     x2 -= x1
 