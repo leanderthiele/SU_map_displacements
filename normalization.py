fixed another bug in normalization.py

normalization.py
@@ -56,7 +56,7 @@ def normalization(mode) :
 
     for run_pair in dataset.run_pairs :
         print('In normalization.py, second loop, {}'.format(run_pair))
-        item = DataItem(run_pair[0])
+        item = DataItem(mode, run_pair[0])
         logdensity = np.log1p( item.density / DENSITY_FACTOR / math.sqrt(var_density) )
         var_logdensity += np.sum(logdensity**2) / logdensity.size
         avg_logdensity += np.mean(logdensity)