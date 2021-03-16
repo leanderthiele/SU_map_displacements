better normalization, fixed lambda captures, more robust against inf/nan

data_item.py
@@ -52,7 +52,7 @@ class DataItem :
         self.is_normalized = False
         self.is_augmented = False
 
-    def normalize(self) :
+    def normalize_for_input(self) :
         # cannot be called after to_torch()
         # TODO in principle, we could think about using the same normalization function
         #      everywhere here. Then it would simply be a collection of magic numbers.
@@ -70,6 +70,19 @@ class DataItem :
         self.is_normalized = True
 
         return self
+
+    def normalize_for_target(self) :
+        # cannot be called after to_torch()
+        
+        assert self.tensor is None
+        assert not self.is_normalized
+
+        # normalize such that the output is in the range [ -1, 1 ]
+        self.displacement /= 0.5 * settings.MAX_BOX_SIZE
+        
+        # Note that delta_L is actually used as an input, but this structure
+        # should be clear
+        self.delta_L = settings.DELTA_L_NORMALIZATIONS[self.mode](self.delta_L)
 
     def __reflect(self, indices) :
         # indices labels the axes that should be reflected
@@ -97,11 +110,12 @@ class DataItem :
         self.displacement = self.displacement[permutation, ...]
 
     def augment_data(self, r) :
-        # cannot be called after to_torch()
+        # cannot be called after to_torch() or normalize
         # performs the data augmentation.
         # r should be some integer
 
         assert self.tensor is None
+        assert not self.is_normalized
         assert not self.is_augmented
 
         r %= 48
@@ -168,6 +182,7 @@ class InputTargetPair :
         # because we need the transformations to be consistent between input and target
         # rand_int should be a callable that produces a random integer,
         # or a random integer
+        
         if rand_int is None :
             r = np.random.default_rng(id(self) % 2**32).integers(2**32)
         elif callable(rand_int) :

data_loader.py
@@ -58,8 +58,8 @@ class Dataset(torch_Dataset) :
 
         return InputTargetPair(DataItem(self.mode, self.run_pairs[run_idx][0]),
                                DataItem(self.mode, self.run_pairs[run_idx][1])) \
-                   .normalize() \
                    .augment_data(augmentation_index) \
+                   .normalize() \
                    .to_torch()
 
     def __getitem__(self, idx) :
@@ -127,6 +127,10 @@ class Batch :
             self.inputs[ii, ...] = data_item.item1.tensor
             self.targets[ii, ...] = data_item.item2.tensor[offset:, ...]
             self.styles[ii, ...] = data_item.styles()
+
+        # sanity check
+        assert torch.min(self.targets).item() >= -1.0
+        assert torch.max(self.targets).item() <= +1.0
 
         return self
 

network.py
@@ -4,7 +4,7 @@ import torch
 import torch.nn as nn
 
 import settings
-from network_utils import Layout, Block, Level
+from network_utils import Activations, Layout, Block, Level
 
 
 class Network(nn.Module) :
@@ -47,7 +47,8 @@ class Network(nn.Module) :
 
         # in the output, we don't want any activation function because we want to
         # map to the entire real line
-        self.collapse = Block(in_layout1, out_layout, activation=False, N_layers=2, residual=False)
+        self.collapse = Block(in_layout1, out_layout,
+                              activation=Activations.OUTPUT, N_layers=2, residual=False)
 
         # we hold the running layout in this variable as we step through the levels
         tmp_layout = deepcopy(in_layout1)

network_utils.py
@@ -25,6 +25,16 @@ class GroupModes(Enum) :
                     # assumes that each direction corresponds to the same number of channels
                     # and the density field, if present, also has this number of channels
 
+class Activations(Enum) :
+    """
+    which activation function to apply
+    """
+    STANDARD = auto() # for the StandardActivation class below
+    NONE = auto() # apply no activation
+    OUTPUT = auto() # apply an activation suitable for the output
+                    # this will apply STANDARD activations for all
+                    # except the very last layer
+
 def crop(x, num=1) :
     """
     removes num rows on each side in each dimension of x
@@ -100,7 +110,7 @@ class Activation_BROKEN(nn.Module) :
         return x
 #}}}
 
-class Activation(nn.Hardshrink) :
+class StandardActivation(nn.Hardshrink) :
     """
     the parent class applied to all channels.
     Needs to respect sign symmetry for the displacement channels.
@@ -108,6 +118,18 @@ class Activation(nn.Hardshrink) :
     """
 #{{{
     def __init__(self, *args) :
+        super().__init__()
+#}}}
+
+class OutputActivation(nn.Tanh) :
+    """
+    suitable for the output of the network, which should fall in the range [-1, 1]
+
+    Note that we choose a relatively expensive function here,
+    but since it is applied only once in the entire network it is completely fine.
+    """
+#{{{
+    def __init__(self) :
         super().__init__()
 #}}}
 
@@ -145,13 +167,13 @@ class Conv3d(nn.Module) :
                              if self.resample is Resample.UP
                              else F.conv3d)(x, w, bias=b, stride=stride, **kwargs)
         else :
-            self.conv_ = (nn.ConvTranspose3d
-                          if self.resample is Resample.UP
-                          else nn.Conv3d)(in_layout.channels, out_layout.channels, 3, stride=stride,
-                                          groups=get_groups(group_mode, in_layout, out_layout),
-                                          bias=True if bias is None else bias)
-            self.conv = lambda x, w, b, **kwargs : \
-                             self.conv_(x)
+            conv = (nn.ConvTranspose3d
+                    if self.resample is Resample.UP
+                    else nn.Conv3d)(in_layout.channels, out_layout.channels, 3, stride=stride,
+                                    groups=get_groups(group_mode, in_layout, out_layout),
+                                    bias=True if bias is None else bias)
+            self.conv = lambda x, w, b, c=conv, **kwargs : \
+                             c(x)
 
     def forward(self, x, w=None, b=None, **kwargs) :
         # note that w, b, and kwargs are only used for the external_weight case
@@ -267,12 +289,14 @@ class Layer(nn.Module) :
     """
 #{{{
     def __init__(self, in_layout, out_layout,
-                       activation=True, batch_norm=False, dropout=False, styles=False, bias=True,
+                       activation=Activations.STANDARD, batch_norm=False, dropout=False, styles=False, bias=True,
                        group_mode=GroupModes.ALL, # do not change if styles=True
                        batch_norm_kw={}) :
         super().__init__()
 
-        self.activation = (nn.Identity if not activation else Activation)(out_layout)
+        self.activation = (nn.Identity if activation is Activations.NONE
+                           else StandardActivation if activation is Activations.STANDARD
+                           else OutputActivation if activation is Activations.OUTPUT)(out_layout)
         self.batch_norm = (nn.Identity if not batch_norm else nn.BatchNorm3d) \
                                 (**batch_norm_kw)
         self.dropout    = (nn.Identity if not dropout else nn.Dropout3d) \
@@ -284,7 +308,8 @@ class Layer(nn.Module) :
                  else Resample.UP if 2*in_layout.resolution == out_layout.resolution \
                  else Resample.DOWN if in_layout.resolution == 2 * out_layout.resolution \
                  else None
-        assert resample is not None, "Incompatible resolutions in=%d out=%d"%(in_layout.resolution, out_layout.resolution)
+        assert resample is not None, "Incompatible resolutions in=%d out=%d"%(in_layout.resolution,
+                                                                              out_layout.resolution)
         common_conv_kwargs = dict(in_layout=in_layout, out_layout=out_layout,
                                   resample=resample, bias=bias)
 
@@ -331,15 +356,15 @@ class Block(nn.Module) :
 #{{{
     def __init__(self, in_layout, out_layout,
                        N_layers=4, residual=True,
-                       activation=True, batch_norm=False, dropout=False, bias=True,
+                       activation=Activations.STANDARD, batch_norm=False, dropout=False, bias=True,
                        batch_norm_kw={}) :
         super().__init__()
 
         self.residual = residual
 
         assert(N_layers > 1)
-        common_layer_kwargs = dict(activation=activation, batch_norm=batch_norm,
-                                   dropout=dropout, bias=bias, batch_norm_kw=batch_norm_kw)
+        common_layer_kwargs = dict(batch_norm=batch_norm, dropout=dropout,
+                                   bias=bias, batch_norm_kw=batch_norm_kw)
 
         if out_layout.density_channels < 0 :
             out_layout.density_channels = in_layout.density_channels * out_layout.channels // in_layout.channels
@@ -350,6 +375,10 @@ class Block(nn.Module) :
                                 out_layout=out_layout,
                                 styles=False if ii==0 else settings.NSTYLES,
                                 group_mode=GroupModes.SINGLE if ii==0 else GroupModes.ALL,
+                                activation=activation \
+                                           if activation is not Activations.OUTPUT or ii != N_layers-1 \
+                                           else \
+                                           Activations.OUTPUT,
                                 **common_layer_kwargs))
         self.layers = nn.ModuleList(layers)
 

startup.py
@@ -46,6 +46,10 @@ def load_normalizations() :
     populates the normalization function dicts
     """
 #{{{
+    # note that we have to *capture* in the lambda,
+    # otherwise we'll always get the same output
+    # regardless of mode
+
     settings.DENSITY_NORMALIZATIONS \
         = settings.DENSITY_NORMALIZATIONS.set(dict())
     settings.DISPLACEMENT_NORMALIZATIONS \
@@ -54,9 +58,9 @@ def load_normalizations() :
         for mode in data_loader.DataModes :
             sigma_displacement, sigma_density, A, B = f[str(mode)]
             settings.DENSITY_NORMALIZATIONS[mode] \
-                = lambda x : A * ( np.log1p(x/sigma_density) - B )
+                = lambda x, s=sigma_density, a=A, b=B : a * ( np.log1p(x/s) - b )
             settings.DISPLACEMENT_NORMALIZATIONS[mode] \
-                = lambda x : x / sigma_displacement
+                = lambda x, s=sigma_displacement : x/s
 
     settings.DELTA_L_NORMALIZATIONS \
         = settings.DELTA_L_NORMALIZATIONS.set(dict())
@@ -66,7 +70,7 @@ def load_normalizations() :
         mean = np.mean(delta_L_arr)
         stddev = np.std(delta_L_arr)
         settings.DELTA_L_NORMALIZATIONS[mode] \
-            = lambda x : (x-mean) / stddev
+            = lambda x, m=mean, s=stddev : (x-m) / s
 #}}}
 
 def set_filenames() :

training.py
@@ -100,6 +100,9 @@ def training_process(rank, world_size) :
         all_epochs_training_loss = np.empty(0)
         all_epochs_validation_loss = np.empty(0)
 
+    global_skip_backward = False
+    skip_backward_list = [False, ] * world_size
+
     for epoch in range(settings.EPOCHS) :
         
         if rank == 0 :
@@ -125,7 +128,12 @@ def training_process(rank, world_size) :
             inputs, targets, styles = data.get_on_device()
 
             # do the forward pass and compute loss
-            optimizer.zero_grad()
+
+            if not global_skip_backward :
+                # we did not encounter infinity/nan during the last forward pass
+                # thus, the optimizer.step method has been called and we can reset
+                # the gradients
+                optimizer.zero_grad()
 
             prediction = ddp_model(inputs, styles)
 
@@ -134,10 +142,27 @@ def training_process(rank, world_size) :
             # update the loss storage
             training_loss[t] = this_training_loss.item()
 
-            # update weights -- this is an implicit synchronization point!
-            this_training_loss.backward()
+            # check whether we need to disable synchronization because
+            # at least one process had a problem
+            this_skip_backward = not np.isfinite(this_training_loss.item())
+            torch.distributed.all_gather_object(skip_backward_list, this_skip_backward)
+            global_skip_backward = any(skip_backward_list)
 
-            optimizer.step()
+            if not global_skip_backward :
+                # update gradients synchronously -- this is an implicit synchronization point!
+                this_training_loss.backward()
+            else :
+                if this_skip_backward :
+                    # we encountered infinity/nan and should throw this loss away
+                    print('encountered invalid loss in rank %d'%rank)
+                else :
+                    # update gradients asynchronously in those processes that did not have a problem
+                    with ddp_model.no_sync() :
+                        this_training_loss.backward()
+
+            if not global_skip_backward :
+                # each process sees the same gradients and can therefore do the same weight updates
+                optimizer.step()
             
             if rank == 0 :
                 print('\tSample %.3d / %d finished in epoch %d, '\