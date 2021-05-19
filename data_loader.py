implemented guess for the target (simply the input displacement field with the correct scaling) -- not sure if this will help

data_loader.py
@@ -119,6 +119,10 @@ class Batch :
                                    device=torch.device('cpu'),
                                    pin_memory=False,
                                    dtype=torch.float32)
+        self.guesses = torch.empty(len(data_items), 3, *[settings.NSIDE,]*3,
+                                   device=torch.device('cpu'),
+                                   pin_memory=False,
+                                   dtype=torch.float32)
         self.styles = torch.empty(len(data_items), settings.NSTYLES,
                                   device=torch.device('cpu'),
                                   pin_memory=False,
@@ -132,6 +136,7 @@ class Batch :
         for ii, data_item in enumerate(data_items) :
             self.inputs[ii, ...] = data_item.item1.tensor
             self.targets[ii, ...] = data_item.item2.tensor[offset:, ...]
+            self.guesses[ii, ...] = data_item.item1.guess
             self.styles[ii, ...] = data_item.styles()
 
         # sanity check
@@ -140,8 +145,9 @@ class Batch :
 
 
     def get_on_device(self) :
-        return self.inputs.to(settings.DEVICE_IDX, non_blocking=True), \
-               self.targets.to(settings.DEVICE_IDX, non_blocking=True), \
+        return self.inputs.to(settings.DEVICE_IDX), \
+               self.targets.to(settings.DEVICE_IDX), \
+               self.guesses.to(settings.DEVICE_IDX), \
                self.styles.to(settings.DEVICE_IDX)
 
 #}}}

network.py
@@ -71,7 +71,7 @@ class Network(nn.Module) :
             print('Network : through-layout = %s'%str(tmp_layout))
 
 
-    def forward(self, x, s) :
+    def forward(self, x, s, guess) :
         x = self.block_in(x, s)
 
         for l in self.levels :
@@ -83,7 +83,19 @@ class Network(nn.Module) :
             x = l.expand(x, s)
 
         x = self.block_out(x, s)
-        x = self.collapse(x, s)
+
+        # TODO maybe this is not the best way to do it!
+        #      Note that guess is in [-1/2, 1/2], so it makes sense to multiply by 2 first
+        #      It should be noted that the arcsin depends on the choice of the OUTPUT activation
+        #      function, so this is information duplication.
+        #
+        #      The main problem is that after adding the `guess' to whatever network output
+        #      we have, we cannot be sure anymore about the periodicity.
+        #      Maybe this whole issues is actually not relevant at all for the vast majority of 
+        #      particles, in which case we could just get rid of the sin etc.
+        #
+        #      --> TEMPORARY, CHANGE LATER
+        x = self.collapse(torch.arcsin(2*guess) + x, s)
 
         return x
 

training.py
@@ -89,10 +89,18 @@ def training_process(rank) :
                 start_time_sample = time()
 
             idx_in_batch += 1
-            
+
             assert isinstance(data, Batch)
 
-            inputs, targets, styles = data.get_on_device()
+            if False :
+                # FIXME for intuition / debugging
+                np.savez('test_%d.npz'%settings.RANK,
+                         inputs=data.inputs.numpy(), targets=data.targets.numpy(),
+                         guesses=data.guesses.numpy(), styles=data.styles.numpy())
+                torch.distributed.barrier()
+                raise RuntimeError('Wrote data to file.')
+            
+            inputs, targets, guesses, styles = data.get_on_device()
 
             # do the forward pass and compute loss
 
@@ -100,7 +108,7 @@ def training_process(rank) :
             # full batch and an invalid loss coincide
             batch_done = idx_in_batch > settings.BATCH_SIZE
 
-            prediction = model(inputs, styles)
+            prediction = model(inputs, styles, guesses)
 
             this_training_loss = loss_fn(prediction, targets)
 